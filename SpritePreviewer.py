909db4c73a17d65c2f979daaf6c18e8069154618
TruLuke <39268805+TruLuke@users.noreply.github.com>
2025-04-17 01:52:48 -0600

    Update 3 & Discontinuing Support
    Fixed organization of layouts and widgets.
    
    Sadly, I am announcing that I will be ending support for a8-sprite-previewer-Luke_Bode.
    If you report any bugs,
    I wont be listening.

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -1,11 +1,12 @@
 import math
 
-from PyQt6.QtCore import Qt
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 
 # GitHub repository name: a8-sprite-previewer-Luke_Bode
+# There's some warnings about converting float to int and how it-
+# might be removed in future pycharm updates, but who cares
 
 # This function loads a series of sprite images stored in a folder with a
 # consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
@@ -34,21 +35,26 @@ class SpritePreview(QMainWindow):
 
     def setupUI(self):
 
+        # menu stuff
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
         file_menu = menubar.addMenu('&File')
+
         pause_action = QAction('&Pause', self)
         pause_action.triggered.connect(self.pause)
         file_menu.addAction(pause_action)
+
         exit_action = QAction('&Exit', self)
         exit_action.triggered.connect(self.quit_program)
         file_menu.addAction(exit_action)
 
         frame = QFrame()
-
+        # main image
         self.image = QLabel()
         self.image.setPixmap(self.frames[self.current_frame_num])
+        self.image.setFixedSize(50,50)
 
+        # fps slider
         self.fps_slider = QSlider()
         self.fps_slider.setMinimum(1)
         self.fps_slider.setMaximum(100)
@@ -57,39 +63,46 @@ class SpritePreview(QMainWindow):
         self.fps_slider.setTickInterval(20)
         self.fps_slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
 
+        # fps label, words and number
         self.fps_label_words = QLabel("Frames per second")
         self.fps_label_words.setFixedWidth(100)
         self.fps_label_words.setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.fps_label_number = QLabel("1")
         self.fps_label_number.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
+        # start/stop button
         self.animate_button = QPushButton("Start")
-        self.animate_button.setFixedWidth(300)
+        self.animate_button.setFixedWidth(200)
         self.animate_button.clicked.connect(self.animate)
 
+        # timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.next_frame)
         self.timer.setInterval(1000/self.fps_amount)
 
+        # main image and fps slider frame and layout
         secondary_frame = QFrame()
         secondary_frame_layout = QHBoxLayout()
-        secondary_frame_layout.setSpacing(200)
-        secondary_frame_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        secondary_frame_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        secondary_frame_layout.setSpacing(50)
         secondary_frame_layout.addWidget(self.image)
         secondary_frame_layout.addWidget(self.fps_slider)
         secondary_frame.setLayout(secondary_frame_layout)
 
+        # fps words and number frame and layout
         fps_frame = QFrame()
         fps_frame_layout = QHBoxLayout()
+        fps_frame_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         fps_frame_layout.addWidget(self.fps_label_words)
         fps_frame_layout.addWidget(self.fps_label_number)
         fps_frame.setLayout(fps_frame_layout)
 
+        # main layout
         main_layout = QVBoxLayout()
         main_layout.addWidget(secondary_frame)
         main_layout.addWidget(fps_frame)
         main_layout.addWidget(self.animate_button)
-        #main_layout.setContentsMargins(20, 20, 320, 320)
+        main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         frame.setLayout(main_layout)
         self.setCentralWidget(frame)
 
